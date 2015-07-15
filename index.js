4c2244efdac1477f6056654d0e1131b1cd2978c6
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2015-07-15 08:35:43 +0200

    adds promises and underscore

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
+"use strict";
+
 var express = require('express');
 var unirest = require('unirest');
 var async = require('async');
+const _ = require('underscore');
 
 var app = express();
 app.set('view engine', 'jade');
 
 const username = 'klausbayrhammer';
-const password = 'xxx';
+const password = process.env.github_pass;
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     console.log(req.query);
     var reponame = req.query.reponame;
     if (reponame !== undefined) {
-        githubApiCall('https://api.github.com/repos/' + reponame + '/stargazers', function (data) {
-            var repos = {};
-            var calls = [];
-            data.body.forEach(function (user) {
-                    calls.push(function (callback) {
-                        githubApiCall('https://api.github.com/users/' + user.login + '/starred', function (data) {
-                            data.body.forEach(function (repo) {
-                                var repo_name = repo.full_name;
-                                repos[repo_name] = (repos[repo_name] || 0) + 1;
-                                console.log(user.login + repo_name);
-                            });
-                            callback(null, user.login);
+        githubApiCall('https://api.github.com/repos/' + reponame + '/stargazers', data =>  {
+            var promises = _.map(data.body, user => {
+                    return new Promise(resolve => {
+                        githubApiCall('https://api.github.com/users/' + user.login + '/starred', data => {
+                            var repo_names = _.map(data.body, (repo) => repo.full_name);
+                            console.log(user.login);
+                            resolve(repo_names);
                         });
-                    })
+                    });
                 }
             );
-            async.parallel(calls, function (err, result) {
-                console.log(repos);
-                res.render('index', {title: data.body.length});
+            Promise.all(promises).then((values) => {
+                var groupedRepoNames = _.countBy(_.flatten(values), v=>v);
+                console.log(groupedRepoNames);
+                res.render('index', {content: JSON.stringify(groupedRepoNames)});
             });
         });
     } else {
@@ -46,7 +45,7 @@ function githubApiCall(url, callback) {
         .end(callback);
 }
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000,() => {
     var host = server.address().address;
     var port = server.address().port;
 
